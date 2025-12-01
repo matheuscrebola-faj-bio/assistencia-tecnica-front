70be8b0de73511f4ee9c59cd895ea911951d9122
matheuscrebola-faj-bio <matheus.cavalcanti@fajbio.com.br>
2025-12-01 08:30:31 -0300

    feat: atualização do IP de conexão

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/', // Ajuste conforme seu backend
+  baseURL: 'http://191.252.218.127:8080/', // Ajuste conforme seu backend
   headers: {
     'Content-Type': 'application/json',
   },
